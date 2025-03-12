5b505c8b4a0a52a065965e7cbc312b6c86cdd8e8
ajay kumar reddy <kalakoti.reddy@coforge.com>
2025-03-12 15:20:40 +0530

    demo

---
+++ Addition.java
@@ -0,0 +1,18 @@
+package com.additiondemo;
+
+/**
+*Author :Kalakoti.Reddy
+*Date   :12-Mar-2025
+*Time   :3:16:18 pm
+*Email  :Kalakoti.Reddy@coforge.com
+*/
+
+public class Addition {
+	public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is " + sum);
+    }
+
+}
